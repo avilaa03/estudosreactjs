f15cff86b6993c577409ec94eec34a709e529036
avilaa03 <lucasdeavila2003@gmail.com>
2023-07-06 17:18:03 -0300

    Estudos React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import SayMyName from "./components/SayMyName"
+import HelloWorld from "./components/HelloWorld"
+import Pessoa from "./components/Pessoa"
+import Frase from "./components/Frase"
+import List from "./components/List"
 
 function App() {
+
+  const newName = "Lucas";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName name="João" />
+      <SayMyName name={newName} />
+      <Pessoa nome="Lucas" idade="20" profissao="Programador" foto="https://via.placeholder.com/150"/>
+      <List />
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,10 @@
+function Item(props) {
+    return (
+        <> 
+        <li>{props.marca}</li>
+        <p>Teste1</p>
+        </>
+    )
+}
+
+export default Item
